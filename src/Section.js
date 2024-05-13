21e19c7b13b6b2e65edd4d104a373cccd9178dab
nitay newman <nitaynewman@gmail.com>
2024-05-13 13:46:00 +0300

    fixing section btn, changing home img and icon, first atempt ad download

--- src/Section.js
+++ src/Section.js
@@ -1,62 +1,48 @@
-import './App.css';
-import Typed from 'typed.js'
 import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js'; // Import Typed from 'typed.js'
 import Navbar from './navbar';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import About from './About'; // Import the About component
 
-       
+export default function Section({ Strings, Title, Paragraph, scrollToAbout }) {
+  const typedRef = useRef(null);
 
-export default function Section({ Strings, Title, Paragraph }) {
-    const typedRef = useRef(null);
-    
-      useEffect(() => {
-        const options = {
-          strings: Strings,
-          typeSpeed: 100,
-          backSpeed: 100,
-          backDelay: 1000,
-          loop: true
-        };
-        const typed = new Typed(typedRef.current, options);
-        return () => {
-          typed.destroy();
-        };
-      }, [Strings]);
-  
-  
-    return(
-      <section className="home">
-        <Navbar />
-          <div className="home-content">
-              <h1>{ Title }</h1>
-              <h3><span ref={typedRef}></span></h3>
-              <p>
-                  { Paragraph }
-              </p>
-              <div className="btn-box">
-                  <a>
-                    <Link path='/' className="LInk">
-                      Information
-                    </Link>
-                  </a>
-                  <a>
-                  <Link to='/contact' className="LInk">
-                    Let's Talk
-                  </Link>
-                  </a>
-              </div>
-          </div>
-          <div className="home-sci">
-              <a>
-              <Link to="/contact" >
-                <li className="bx bxl-gmail"></li>
-              </Link>
-              </a>
-              <a href="https://github.com/nitaynewman?tab=repositories"><i className="bx bxl-github"></i></a>
-              <a href="/"><i className="bx bxl-linkedin"></i></a>
-          </div>
-          <span className="home-imgHover"></span>
-      </section>
-    )
-  }
-  
+  useEffect(() => {
+    const options = {
+      strings: Strings,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+    };
+    const typed = new Typed(typedRef.current, options);
+    return () => {
+      typed.destroy();
+    };
+  }, [Strings]);
+
+  return (
+    <section className="home">
+      <Navbar />
+      <div className="home-content">
+        <h1>{Title}</h1>
+        <h3><span ref={typedRef}></span></h3>
+        <p>{Paragraph}</p>
+        <div className="btn-box">
+          <button onClick={scrollToAbout} className="LInk">Information</button> {/* Changed Link to button */}
+          <Link to='/contact' className="LInk">Let's Talk</Link>
+        </div>
+      </div>
+      <div className="home-sci">
+        <a>
+          <Link to="/contact">
+            <li className="bx bxl-gmail"></li>
+          </Link>
+        </a>
+        <a href="https://github.com/nitaynewman?tab=repositories"><i className="bx bxl-github"></i></a>
+        <a href="/"><i className="bx bxl-linkedin"></i></a>
+      </div>
+      <span className="home-imgHover"></span>
+    </section>
+  );
+}
--- src/python/pyCarousel.js
+++ src/python/pyCarousel.js
@@ -4,31 +4,28 @@ import './slick.css'
 import './slick-theme.css'
 import './python.css'
 import '../App.css'
-// import 'react-html5video/dist/index'
 import ReactPlayer from 'react-player'
 
-
-
 function Arrow(props) {
-  
     const { className, style, onClick } = props;
     return (
         <div 
-        className={className}
-        style={{ ...style, display: 'block' }}
-        onClick={onClick}
-        >
-            
-        </div>
+            className={className}
+            style={{ ...style, display: 'block' }}
+            onClick={onClick}
+        ></div>
     )
 }
 
 function Carousel({ data, title }) {
-  if (!data || data.length === 0) {
-    // Handle the case where data is undefined or empty
-    return <div>No data available</div>;
-  }
-    const Settings = {
+    if (!data || data.length === 0) {
+        // Handle the case where data is undefined or empty
+        return <div>No data available</div>;
+    }
+
+
+    
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
@@ -38,59 +35,57 @@ function Carousel({ data, title }) {
         nextArrow: <Arrow />,
         prevArrow: <Arrow />,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 900,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 700, 
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 700, 
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
         ]
     };
-    
+
     return (
         <div className="container">
             <h2 style={{fontSize: "40px"}}>{title}</h2>
-            <Slider {...Settings} >
+            <Slider {...settings}>
                 {data.map((item) => (
                     <div key={item.id} className="card">
                         <div className="card-body">
                             <h3>{item.title}</h3>
                             <div className="card-container">
-                              <div className="sides">
-                                <p>{item.paragraph}</p>
-                                <button>
-                                  <a className='LINK' href={`${item.git_url}`} target="_blank" rel="noopener noreferrer">GitHub-Code</a>
-                                  </button>
-                                <button href={item.git_url}>Download</button>
-                              </div>
-                              <div className="sides video-container">
-                                <ReactPlayer 
-                                className='video'
-                                url={item.video}
-                                controls={false}
-                                loop={true}
-                                playing={true}
-                                muted={true}
-                                />
-                              </div>
+                                <div className="sides">
+                                    <p>{item.paragraph}</p>
+                                    <button onClick={() => window.open(item.git_url, '_blank')} className='LINK'>GitHub-Code</button>
+                                    <button onClick={() => window.open(item.git_url, '_blank')}>Download</button>
+                                </div>
+                                <div className="sides video-container">
+                                    <ReactPlayer 
+                                        className='video'
+                                        url={item.video}
+                                        controls={false}
+                                        loop={true}
+                                        playing={true}
+                                        muted={true}
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>
